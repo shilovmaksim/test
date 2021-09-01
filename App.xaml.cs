fea01addc29a12a13bcaf2d6e641c4736231a87a
Maksim Shilov <maksim.shilov@akvelon.com>
2021-09-01 22:00:35 +0300

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppCenter.Start("7c778b58-db0e-4512-923f-74e07ff956cb",
+                   typeof(Analytics), typeof(Crashes));
+        }
+    }
+}
